6e2c8f1fac11a8ed522f394605dc66367003cff3
AaronPGao <163357467+AaronPGao@users.noreply.github.com>
2025-02-08 15:17:59 -0500

    changed formatting

--- streamlit-app/app/Home.py
+++ streamlit-app/app/Home.py
@@ -14,20 +14,23 @@ st.markdown("""
         opacity: 1;
       }
     }
+    img {
+        border-radius: 20px;
+    }
     .header-title {
-        font-size: 60px;
+        font-size: 80px !important;
         font-weight: bold;
         color: #FF4A4A;
         text-align: center;
         margin-bottom: 20px;
     }
     .description {
-        font-size: 20px;
+        font-size: 21px !important;
         color: #555555;
         text-align: center;
-        margin-bottom: 30px;
+        margin-bottom: 10px;
         opacity: 0;
-        transform: translateY(20px);
+        
         animation: fadeIn 2s ease-in forwards;
     }
     .subheading {
@@ -35,15 +38,19 @@ st.markdown("""
         opacity: 0;
         transform: translateY(20px);
         animation: fadeIn 2.5s ease-in forwards;
+        margin-bottom: 10px !important;
      
     }
     .stButton button {
+        margin-top: 30px;
         background-color: #FF4A4A;
         color: white;
         border-radius: 10px;
-        padding: 10px 20px;
-        font-size: 18px;
+        padding: 22px 40px;
+        font-size: 30px !important;
         font-weight: bold;
+        min-width: 200px;
+        white-space: nowrap;
         border: none;
         cursor: pointer;
     }
@@ -57,10 +64,10 @@ image_path = os.path.join(os.getcwd(), "nonprofit.jpg")
 # Centered Title
 st.markdown('<h1 class="header-title">EduConnect.AI</h1>', unsafe_allow_html=True)
 
-col1, col2, col3 = st.columns([1, 3, 1])  # Adjust proportions as needed
+#col1, col2, col3 = st.columns([1, 3, 1])  # Adjust proportions as needed
 
-with col2:  # Center the image in the second column
-    st.image("./app/images/nonprofit.jpg", use_container_width=True)
+#with col2:  # Center the image in the second column
+st.image("./app/images/nonprofit.jpg", use_container_width=True)
 
 st.markdown('<h2 class="subheading">What is EduConnect.AI?</h1>', unsafe_allow_html=True)
 
@@ -70,10 +77,9 @@ st.markdown('<p class="description">EduConnect.AI is an AI solution that helps c
 st.markdown('<h2 class="subheading">How do you use EduConnect.AI?</h1>', unsafe_allow_html=True)
 
 # Centered Description
-st.markdown('<p class="description">EduConnect.AI starts by asking the user a few preliminary questions before giving suggestions. The user then rates the suggestions EduConnect.AI has given. These ratings are then refined to give better and better suggestions so that you can find your ideal non-profit! Finally, EduConnect will help write a message to reach out to the non-profit to begin your connection. Get started by clicking the button below!</p>', unsafe_allow_html=True)
+st.markdown('<p class="description">EduConnect.AI begins by asking you a few initial questions to tailor its suggestions. You can rate the recommendations provided, and these ratings help refine our AI agent to deliver increasingly accurate suggestions, guiding you to your ideal non-profit. Once you find the perfect match, EduConnect will assist in drafting a message to help you connect with the organization. Ready to start? Click the button below!</p>', unsafe_allow_html=True)
 
-
-col1,col2,col3 = st.columns([1.4    ,1.,1.])
+col1,col2,col3 = st.columns([1.2,1.,1.])
 with col2:
     if st.button("Start Connecting!"):
         st.switch_page("pages/chat.py")  # Ensure this page exists in your Streamlit app
